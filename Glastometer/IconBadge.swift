7160ee893e909d90be75c785ddfb0c62dd66350d
Joe <j.neale@mac.com>
2014-10-27 23:17:10 +0000

    Added function to IconBadge to create array of UILocalNotifications - this has test code in at the moment to see how many notifications can be scheduled - to be changed to days remaining

--- Glastometer/IconBadge.swift
+++ Glastometer/IconBadge.swift
@@ -20,6 +20,9 @@ public class IconBadge
     
     func setBadge()
     {
+        //create an oject of UIApplication, must remember to 'import UIKit' as we're not in a ViewController.
+        var application: UIApplication = UIApplication.sharedApplication()
+        
         // Get the icon badge switch state from NSUserDefaults
         var showIconBadge = defaults.objectForKey("showIconBadge") as? Bool
         if (showIconBadge!)
@@ -31,15 +34,76 @@ public class IconBadge
             }
             thisCountdown.Config(targetDate!)
             
+            application.scheduledLocalNotifications = localNotificationsStartingTomorrow()
+            
             // Set the icon badge
-            //create an oject of UIApplication, must remember to 'import UIKit' as we're not in a ViewController.
-            var application: UIApplication = UIApplication.sharedApplication()
             application.applicationIconBadgeNumber = thisCountdown.RemainingDays().days
+        }
+        else
+        {
+            application.applicationIconBadgeNumber = 0 //Hide the badge if this option is turned off.
         }
     }
     
     
     
+    func localNotificationsStartingTomorrow() -> [UILocalNotification]
+    {
+        var localNotifications: [UILocalNotification] = []
+    
+        for var i = 0; i < 365; i++
+        {
+            var notification: UILocalNotification = UILocalNotification()
+            notification.applicationIconBadgeNumber = i
+            notification.fireDate = NSDate(timeIntervalSinceNow: (NSTimeInterval)(1 + (3 * i)))
+            UIApplication.sharedApplication().scheduleLocalNotification(notification)
+            
+            localNotifications.append(notification)
+        }
+        
+        return localNotifications
+    }
+    
+    /*
+    
+    
+    -(NSArray *) localNotificationsStartingTomorrow{
+    
+    //Create an array of 64 notifications (64 is the maximum allowed per application)
+    NSMutableArray *localNotifications = [[NSMutableArray alloc] initWithCapacity:64];
+    
+    for (NSUInteger i = 0; i < 64; i++){
+    //Create a new local notification
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    notification.hasAction = NO;
+    
+    //Create today's midnight date
+    NSCalendar *calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateComponents *todaysDateComponents = [calendar components:(NSEraCalendarUnit | NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) fromDate:[NSDate date]];
+    NSDate *todaysMidnight = [calendar dateFromComponents:todaysDateComponents];
+    
+    //Create the fire date
+    NSDateComponents *addedDaysComponents = [[NSDateComponents alloc] init];
+    addedDaysComponents.day = i;
+    NSDate *fireDate = [calendar dateByAddingComponents:addedDaysComponents toDate:todaysMidnight options:0];
+    
+    //Set the fire date and time zone
+    notification.fireDate = fireDate;
+    notification.timeZone = [NSTimeZone systemTimeZone];
+    
+    //Set the badge number
+    notification.applicationIconBadgeNumber = [self remainingDays] - (i + self.fudgeFactor);
+    //NSLog(@"Notification: %@, Days %lu", fireDate, [self remainingDays] - (i + self.fudgeFactor));
+    
+    //Add the notification to the array
+    [localNotifications addObject:notification];
+    }
+    
+    return [localNotifications copy];
+    }
+
+    
+    */
     
     
     
