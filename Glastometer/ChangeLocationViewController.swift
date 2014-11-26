b402129291ee5a02b989421d18f381e5bbdc63ed
Joe <j.neale@mac.com>
2014-11-26 13:15:57 +0000

    Added MapView to change location cell on settings table. need to fix auto layout issues.

---
+++ Glastometer/ChangeLocationViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeLocationViewController.swift
+//  Glastometer
+//
+//  Created by Joe on 26/11/2014.
+//  Copyright (c) 2014 Joe. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class ChangeLocationViewController : UIViewController, MKMapViewDelegate
+{
+    @IBOutlet weak var mapView: MKMapView!
+
+
+}
