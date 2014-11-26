65ee3ae2d4a28f35181a97ba3f22e2e3437df248
Joe <j.neale@mac.com>
2014-11-26 23:08:20 +0000

    Fixed auto layout of MapView Added MyAnnotation class When map view is loaded, a pin is dropped on the event and the map zooms to centre on event

--- Glastometer/ChangeLocationViewController.swift
+++ Glastometer/ChangeLocationViewController.swift
@@ -14,5 +14,57 @@ class ChangeLocationViewController : UIViewController, MKMapViewDelegate
 {
     @IBOutlet weak var mapView: MKMapView!
 
+    let longPressRec = UILongPressGestureRecognizer()
+    
+    override func viewDidLoad() {
+        longPressRec.addTarget(self, action: "longPressedView")
+        mapView.addGestureRecognizer(longPressRec)
+        
+        addPinToMap()
+    }
+    
+    
+    func addPinToMap(){
+        let location = CLLocationCoordinate2D(latitude: 51.155543, longitude: -2.586368)
+        let annotation = MyAnnotation(coordinate: location, title: SavedSettings().eventName, subtitle: "")
+        mapView.addAnnotation(annotation)
 
+        setCentreOfMapToLocation(location)
+    }
+    
+    
+    func setCentreOfMapToLocation(location: CLLocationCoordinate2D){
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
+    func longPressedView(){
+        //Put the code in here to drop a new pin when long pressed complete.
+    }
+    
+/*
+    - (void)viewDidLoad
+    {
+    [super viewDidLoad];
+    
+    [self.mapView setDelegate:self];
+    
+    // Do any additional setup after loading the view.
+    self.settings = [SettingsClass sharedSingleton];
+    
+    //This adds the gesture recognizer to the mapView for a long press... ie to drop a pin on the map... see handleLongPress method below
+    UILongPressGestureRecognizer *lpgr = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(handleLongPress:)];
+    lpgr.minimumPressDuration = 1.5; //user needs to press for 1.5 seconds
+    [self.mapView addGestureRecognizer:lpgr];
+    }
+*/
+    
+    /* This make a popup box apear.
+    let tapAlert = UIAlertController(title: "Long Pressed", message: "You just long pressed the long press view", preferredStyle: UIAlertControllerStyle.Alert)
+    tapAlert.addAction(UIAlertAction(title: "OK", style: .Destructive, handler: nil))
+    self.presentViewController(tapAlert, animated: true, completion: nil)
+    */
+    
 }
---
+++ Glastometer/MyAnnotation.swift
@@ -0,0 +1,24 @@
+//
+//  MyAnnotation.swift
+//  Glastometer
+//
+//  Created by Joe on 26/11/2014.
+//  Copyright (c) 2014 Joe. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MyAnnotation : NSObject, MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D = CLLocationCoordinate2DMake(0,0)
+    var title: String!
+    var subtitle: String!
+    
+    init(coordinate: CLLocationCoordinate2D, title: String, subtitle: String) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+        super.init()
+    }
+}
